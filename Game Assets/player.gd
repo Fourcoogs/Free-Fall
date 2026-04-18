74e1df88aba8ccd0f93531360275f1b5304398de
Fourcoogs <josephstalin.will@gmail.com>
2026-04-17 20:20:50 -0500

    Dictator Added
    Most basic player scene added

---
+++ Game Assets/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+@export var max_health: int = 5
+var health: int = 5
+#var weapon: Resource
+var attack_ready: bool = true
+#var is_unarmed: bool
+@export var speed: float = 10000
+#var pickups: Array = []
+var alive: bool = true
+
+func _ready():
+	health = max_health
+	pass
+
+func _process(delta: float) -> void:
+	if alive:
+		look_at(get_global_mouse_position())
+		if Input.is_action_pressed("Attack") and attack_ready:
+			pass
+
+func movement_method(delta):
+	if alive:
+		var input_dir = Input.get_vector("Left", "Right", "Up", "Down")
+		velocity = input_dir * speed * delta
+
+func _physics_process(delta: float) -> void:
+	movement_method(delta)
+	move_and_slide()
+
+func damage(amount, type, angle):
+	if alive:
+		health -= amount
+		if health <= 0:
+			alive = false
+			rotation_degrees = angle - 180
+
+func equip():
+	pass
